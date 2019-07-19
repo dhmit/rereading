22a3ed8c3306c1525d4b32a28e4d452556ba9312
Sophia Zhi <sophiazhi@gmail.com>
2019-07-19 11:04:16 -0400

    some more backend setup

--- backend/baby_shoes_api/baby_shoes/models.py
+++ backend/baby_shoes_api/baby_shoes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class BabyShoes(models.Model):
+    story = models.TextField()
+    context_1 = models.TextField()
+    question_1 = models.TextField()
+    word_limit_1 = models.IntegerField()
+    context_2 = models.TextField()
+    question_2 = models.TextField()
+    word_limit_2 = models.IntegerField()

972228c85743d71fa0639fffb20448e4e54cc9ed
Sophia Zhi <sophiazhi@gmail.com>
2019-07-22 15:44:31 -0400

    added context and question to admin

--- backend/baby_shoes_api/baby_shoes/admin.py
+++ backend/baby_shoes_api/baby_shoes/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import BabyShoes
+from .models import BabyShoes, Context, Question
 
 # Register your models here.
-admin.site.register(BabyShoes)
+# admin.site.register(BabyShoes)
+# admin.site.register(Context)
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+class ContextInline(admin.TabularInline):
+    model = Context
+    extra = 1
+
+
+class BabyShoesAdmin(admin.ModelAdmin):
+    model = BabyShoes
+    inlines = [ContextInline, QuestionInline]
+
+admin.site.register(BabyShoes, BabyShoesAdmin)

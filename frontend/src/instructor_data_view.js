12e243a8290830d31022ed25d3c8906b9da75037
meesuekim <meesue@mit.edu>
2019-07-25 12:48:39 -0400

    instructor data view page

--- frontend/src/instructor_data_view.js
+++ frontend/src/instructor_data_view.js
@@ -1,22 +1,41 @@
-// import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
-//
-// export default function InstructorDataView() {
-//     return (
-//         <div className="container">
-//             <h1>Instructor Data View</h1>
-//             <p>
-//                 This is the instructor data view page.
-//             </p>
-//         </div>
-//     )
-// }
-
 import React from 'react'
 
 class InstructorPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+        };
+    }
+    async componentDidMount() {
+        try {
+            const res = await fetch('http://127.0.0.1:8000/api/');
+            const data = await res.json();
+            this.setState({
+                data
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     render() {
-        return <h1>Instructor Data View</h1>
+        return (
+            <div>
+                {this.state.data.map(item => (
+                    <div key={item.id}>
+                        <h1>{item.story}</h1>
+                        <h3>{item.contexts[0]}</h3>
+                        <p>{item.questions[0]['text']}</p>
+                        <p>{item.questions[1]['text']}</p>
+                        <h3>{item.contexts[1]}</h3>
+                        <p>{item.questions[0]['text']}</p>
+                        <p>{item.questions[1]['text']}</p>
+
+                    </div>
+                ))}
+            </div>
+        );
     }
 }
 

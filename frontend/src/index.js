6e583f5db053e8118f5875c0fad085210aa19039
Ryaan Ahmed <rahmed@mit.edu>
2019-09-06 13:03:19 -0400

    remove unused Link import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 

2bd413bc6483ae4a702b39152a174b345e11f460
Ivy Mao <maoivy01@gmail.com>
2019-09-13 16:12:43 -0400

    Update analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -28,7 +28,20 @@ def run_analysis():
     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
     student_data = load_data_csv(csv_path)
     # TODO: do something with student_data that's not just printing it!
+
+    ad = {}
+    story = {}
+    for elem in student_data:
+        if elem['context'] == 'This is an ad.':
+            sentiment = elem['response']
+            if sentiment not in ad:
+                ad[sentiment] += 1
+
+
+
+
     print(student_data)
+    print(type(student_data[0]))
 
 
 if __name__ == '__main__':

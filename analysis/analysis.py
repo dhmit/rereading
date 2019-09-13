c2834486a2bf2b90e16dc603c0d688620fae5dec
Kitty Zeng <kzeng@mit.edu>
2019-09-13 13:55:21 -0400

    added our brainstorm to the analysis file

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -23,12 +23,29 @@ def load_data_csv(csv_path: Path):
             out_data.append(row)
     return out_data
 
+'''
+Analysis ideas
+- Aggregating connotations of words
+- Sort words into different tones or moods
+- What are the most common responses?
+- Words and frequencies
+- The first reading response vs. the second reading response
+    - how often does the response stay the same vs. change?
+    - how does the connotation of the response change?
+- Compare lengths of readings
+- Number of rereadings (and avg etc.)
+
+Data collection ideas
+- What happens if we reverse the order of the contexts?
+
+'''
+
 
 def run_analysis():
     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
     student_data = load_data_csv(csv_path)
     # TODO: do something with student_data that's not just printing it!
-    print(student_data)
+    print(student_data[0])
 
 
 if __name__ == '__main__':

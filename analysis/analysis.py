6d1f4768c7f41cb3a34691cb1e40e0f663794231
Sirena Yu <yusirena4869@gmail.com>
2019-09-13 15:59:54 -0400

    Extracted yes_id and no_id from the student data
    we
    -extracted student ids for which the student answered yes vs no
    -started on extracting the words in student responses

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -31,8 +31,31 @@ def run_analysis():
     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
     student_data = load_data_csv(csv_path)
     # TODO: do something with student_data that's not just printing it!
-    print(student_data)
+#    print(student_data[0]['question'])
+
+    # Iterate through all records, and separate
+    yes_id = []
+    no_id = []
+    for response in student_data:
+        if (response['question'].find('encountered this text') == 0
+                and response['context'].find('This is an ad.') == 0):
+            if response['response'].lower().find('yes')  == -1:
+                no_id.append(response['student_id'])
+            else:
+                yes_id.append(response['student_id'])
+    print(yes_id)
+    print(no_id)
+
+    ad_yes_words = []
+    ad_no_words = []
+    for element in student_data:
+        if element['question'].find('In one word') == 0:
+
+
+
+
 
 
 if __name__ == '__main__':
     run_analysis()
+

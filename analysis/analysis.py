914eb2bd68fdb865d2723436a932a4dc559c73a9
wilke0818 <39885245+wilke0818@users.noreply.github.com>
2019-09-13 16:53:09 -0400

    Update analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -31,6 +31,34 @@ def run_analysis():
     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
     student_data = load_data_csv(csv_path)
     # TODO: do something with student_data that's not just printing it!
+    average_ad_time = 0
+    average_short_story_time = 0
+    ad_count = 0
+    short_story_count = 0
+    for student_data_dictionary in student_data:
+        if student_data_dictionary['question'].find("feel") != -1:
+
+                average_views = 0
+                for view_time in student_data_dictionary['views']:
+                    average_views += view_time
+                if len(student_data_dictionary['views']) != 0:
+                    ad_count += 1
+                    average_ad_time += average_views/len(student_data_dictionary['views'])
+            else:
+
+                average_views = 0
+                for view_time in student_data_dictionary['views']:
+                    average_views += view_time
+                if len(student_data_dictionary['views']) != 0:
+                    short_story_count += 1
+                    average_short_story_time += average_views / len(student_data_dictionary['views'])
+    average_short_story_time /= short_story_count
+    average_ad_time /= ad_count
+
+    print("Number of people who reread the text thinking it was an ad: " + str(ad_count) + ".")
+    print("Their average reread time for the first question was " + str(round(average_ad_time,2)) + " seconds.")
+    print("Number of people who reread the text thinking it was a short story: " + str(short_story_count) + ".")
+    print("Their average reread time for the first question was " + str(round(average_short_story_time,2)) + " seconds.")
     print(student_data)
 
 

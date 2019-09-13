8c227a2aa040a1a79f4f153a3d6cb323ff009b73
Catherine Mei <meic1212@mit.edu>
2019-09-13 15:58:43 -0400

    Started working on final counts of q1 ad

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -27,7 +27,40 @@ def load_data_csv(csv_path: Path):
 def run_analysis():
     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
     student_data = load_data_csv(csv_path)
+
     # TODO: do something with student_data that's not just printing it!
+q1_final_ad = []
+q2_final_ad = []
+q3_final_ad = []
+q1_final_short = []
+q2_final_short = []
+q3_final_short = []
+zero_counter = 0
+one_counter = 0
+two_counter = 0
+three_counter = 0
+four_counter = 0
+five_above= 0
+
+for x in q1_ad:
+    if(x==0):
+        zero_counter+=1
+    elif(x==1):
+        one_counter+=1
+    elif (x == 2):
+        two_counter += 1
+    elif (x == 3):
+        three_counter += 1
+    elif (x == 4):
+        four_counter += 1
+    else:
+        five_above +=1
+
+
+
+
+
+
     print(student_data)
 
 

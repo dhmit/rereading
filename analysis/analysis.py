ea717c73b2f00c882c193e3b2b649ce678ef0acf
Ryaan Ahmed <rahmed@mit.edu>
2019-09-13 12:35:07 -0400

    add template for initial data analysis

---
+++ analysis/analysis.py
@@ -0,0 +1,34 @@
+"""
+
+Analysis.py - initial analyses for dhmit/rereading
+
+"""
+
+import csv
+from pathlib import Path
+
+
+def load_data_csv(csv_path: Path):
+    """
+    Takes the path to a csv file, reads it, and returns its
+    content as a list of OrderedDicts
+    :param Path csv_path: path to the CSV file
+    :return: List[dict]
+    """
+    out_data = []
+    with open(str(csv_path)) as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            out_data.append(row)
+    return out_data
+
+
+def run_analysis():
+    csv_path = Path('data', 'rereading_data_2019-09-13.csv')
+    student_data = load_data_csv(csv_path)
+    # TODO: do something with student_data that's not just printing it!
+    print(student_data)
+
+
+if __name__ == '__main__':
+    run_analysis()

c9c5f03c0be08acdfed1d658a8cd8901f98c2d00
Justice Vidal <jmvidal@mit.edu>
2019-09-13 17:19:01 -0400

    justice_christina_rereading_data_analysis_9_13_19.py
    Created framework for quantifying responses based on number of views

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -30,8 +30,44 @@ def load_data_csv(csv_path: Path):
 def run_analysis():
     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
     student_data = load_data_csv(csv_path)
-    # TODO: do something with student_data that's not just printing it!
-    print(student_data)
+    # TODO: do something with student_data that's not just printing it
+    one_word_response = []
+    story_response = []
+    response_dictionary = {}
+    for dictionary in student_data:
+        if (dictionary["context"] == "This is an ad."
+                and dictionary["question"] == "In one word, how does this text make you feel?"):
+            one_word_response.append(dictionary["response"])
+            len_views = len(dictionary["views"])
+            if len_views == 0:
+                print('0')
+            elif len_views in response_dictionary.keys():
+                response_dictionary[len_views].append(dictionary["response"])
+            else:
+                response_dictionary.update({len_views: [dictionary["response"]]})
+        if (dictionary["context"] == "This is actually a short story."
+                and dictionary["question"] == "In one word, how does this text make you feel?"):
+            story_response.append(dictionary["response"])
+    one_word_response_sad_number = 0
+    story_response_sad_number = 0
+    for response in one_word_response:
+        if response.lower() == "sad":
+            one_word_response_sad_number += 1
+    for response in story_response:
+        if response.lower() == "sad":
+            story_response_sad_number += 1
+    print(one_word_response)
+    print(one_word_response_sad_number)
+    print(story_response)
+    print(story_response_sad_number)
+    views = []
+    for dictionary in student_data:
+        views.append(len(dictionary["views"]))
+    print(views)
+    print("max views", max(views))
+    print("average views:", sum(views) / len(views))
+    print(response_dictionary)
+    # print(student_data)
 
 
 if __name__ == '__main__':

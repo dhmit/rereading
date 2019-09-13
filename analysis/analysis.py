75f5dda14a213faa327e85acb65ff9dfd1090eb9
kidi <kidistadamu7@gmail.com>
2019-09-13 14:33:06 -0400

    implement run analysis function
    Creates an analysis function that compares the average view time for ads vs stories

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -25,37 +25,10 @@ def load_data_csv(csv_path: Path):
 
 
 def run_analysis():
-    """This analysis function compares the average view time for when the responder
-     was told the text was an ad versus a story"""
-
     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
     student_data = load_data_csv(csv_path)
+    # TODO: do something with student_data that's not just printing it!
+    print(student_data)
 
-    ad_views = 0
-    ad_views_counter = 0
-    story_views = 0
-    story_views_counter = 0
-
-    for elem in student_data:
-        if elem['context'] == 'This is an ad.':
-            if len(elem['views']) == 0:
-                ad_views += 0
-                ad_views_counter += 1
-            else:
-                ad_views += elem['views'][0]
-                ad_views_counter += 1
-        else:
-            if len(elem['views']) == 0:
-                story_views += 0
-                story_views_counter += 1
-            else:
-                story_views += elem['views'][0]
-                story_views_counter += 1
-
-    average_ad = ad_views/ ad_views_counter
-    average_story = story_views/ story_views_counter
-
-    print("The average viewing time for when the responder was told it was an ad was ", average_ad,
-           "while the average viewing time for when the responder was told it was a story was ", average_story)
 if __name__ == '__main__':
     run_analysis()

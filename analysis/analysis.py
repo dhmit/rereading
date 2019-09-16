24ceff81f0876b97dd7fa965dfdf2ce36128a5e9
SageTLMax <samaxw@mit.edu>
2019-09-17 04:20:55 +0900

    Update analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -27,16 +27,52 @@ def load_data_csv(csv_path: Path):
 def run_analysis():
     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
     student_data = load_data_csv(csv_path)
-    # Arrays for each type of context
+    """"
+    This program shows how many times students had to reread the text depending
+    on the context (ad or short story) and the question.
+    """
+
+    # Question 1
     q1_ad = []
     q1_short = []
     for row in student_data:
-        # "ad" signifies the "This is an ad." context
         # "encountered" signifies the "Have you encountered this text before?" question
+        # "ad" signifies the "This is an ad." context
+        # "short" signifies the "This is a short story." context
         if "ad" in row['context'] and "encountered" in row['question']:
             view_count = len(row['views'])
             q1_ad.append(view_count)
-    print(q1_ad)
+        elif "short" in row['context'] and "encountered" in row['question']:
+            view_count = len(row['views'])
+            q1_short.append(view_count)
+
+    # Question 2
+    q2_ad = []
+    q2_short = []
+    for row in student_data:
+        # "one" signifies the "In one word, how does this make you feel?" question
+        # "ad" signifies the "This is an ad." context
+        # "short" signifies the "This is a short story." context
+        if "ad" in row['context'] and "one" in row['question']:
+            view_count = len(row['views'])
+            q2_ad.append(view_count)
+        elif "short" in row['context'] and "one" in row['question']:
+            view_count = len(row['views'])
+            q2_short.append(view_count)
+
+    # Question 3
+    q3_ad = []
+    q3_short = []
+    for row in student_data:
+        # "three" signifies the "In three words or fewer, what is this text about?" question
+        # "ad" signifies the "This is an ad." context
+        # "short" signifies the "This is a short story." context
+        if "ad" in row['context'] and "three" in row['question']:
+            view_count = len(row['views'])
+            q3_ad.append(view_count)
+        elif "short" in row['context'] and "three" in row['question']:
+            view_count = len(row['views'])
+            q3_short.append(view_count)
 
 
 if __name__ == '__main__':

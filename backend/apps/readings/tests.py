886204e04488ca5505ddb8b1bed597051eff7135
Ryaan Ahmed <rahmed@mit.edu>
2019-09-18 17:53:22 -0400

    workflow tweaks

--- backend/apps/readings/tests.py
+++ backend/apps/readings/tests.py
@@ -4,4 +4,4 @@ from django.test import TestCase
 class RereadingTests(TestCase):
     def test_is_this_on(self):
         """ Trivial test to make sure the testing system is working """
-        self.assertTrue(True)
+        self.assertTrue(2+2 == 4)

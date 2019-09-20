5e55b5b3a860aaaf7741a6244027c46adc8137f1
Ivy Mao <maoivy01@gmail.com>
2019-09-20 16:29:20 -0400

    more readable

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -3,9 +3,9 @@
 Analysis.py - initial analyses for dhmit/rereading
 
 """
-
 import csv
 from pathlib import Path
+import unittest
 
 
 def load_data_csv(csv_path: Path):
@@ -25,26 +25,43 @@ def load_data_csv(csv_path: Path):
 
 
 def compile_response(student_data, question):
+    """
+    Returns answers as a dictionary with context as the keys and another dictionary containing each
+    response and their frequency as the value.
+    :param student_data: list of OrderedDicts, set of responses
+    :param question: string, question
+    :return: dictionary
+    """
     data = {}
     for elem in student_data:
-        if elem['question'] == question:
-            if elem['context'] not in data:
-                data[elem['context']] = {elem['response']: 1}
+        student_question = elem['question']
+        student_response = elem['response']
+        question_context = elem['context']
+        if student_question == question:
+            if question_context not in data:
+                data[question_context] = {student_response: 1}
             else:
-                if elem['response'] in data[elem['context']]:
-                    data[elem['context']][elem['response']] += 1
+                if student_response in data[question_context]:
+                    data[question_context][student_response] += 1
                 else:
-                    data[elem['context']][elem['response']] = 1
+                    data[question_context][student_response] = 1
     return data
 
 
 def common_response(student_data, question, context):
-    max = 0
+    """
+    Returns the most common response
+    :param student_data:
+    :param question:
+    :param context:
+    :return:
+    """
+    max_freq = 0
     max_response = ''
     response_dict = compile_response(student_data, question)
     for response in response_dict[context]:
-        if response_dict[context][response] > max:
-            max = response_dict[context][response]
+        if response_dict[context][response] > max_freq:
+            max_freq = response_dict[context][response]
             max_response = response
     return max_response
 
@@ -55,6 +72,32 @@ def run_analysis():
     print(common_response(student_data, "In one word, how does this text make you feel?", "This is an ad."))
 
 
+class TestAnalysisMethods(unittest.TestCase):
+    def setUp(self):
+        test_data_path = Path('data', 'test_data.csv')
+        self.test_student_data = load_data_csv(test_data_path)
+        self.default_student_data = [  # model default values
+            {
+                'id': 0,
+                'question': '',
+                'context': '',
+                'response': '',
+                'views': [],
+                'student_id': 0,
+                'scroll_ups': 0,
+            }
+        ]
+
+
+def test_common_response(self):
+    most_common_response = common_response(self.test_student_data, "In one word, how does this text make you feel?", "This is an ad.")
+    self.assertEqual(most_common_response, "Happy")
+
+    # check we don't crash on the defaults from the model!
+    most_common_response = common_response(self.default_student_data, "In one word, how does this text make you feel?", "This is an ad.")
+    self.assertEqual(most_common_response, '')
+
+
 if __name__ == '__main__':
     run_analysis()
-
+    unittest.main()  # run the tests

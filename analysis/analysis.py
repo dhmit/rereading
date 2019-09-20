95787a2d97262e5eef6971261d460a2100588aee
SageTLMax <samaxw@mit.edu>
2019-09-21 02:48:38 +0900

    Update analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -24,14 +24,15 @@ def load_data_csv(csv_path: Path):
     return out_data
 
 
-def run_analysis():
-    csv_path = Path('data', 'rereading_data_2019-09-13.csv')
-    student_data = load_data_csv(csv_path)
+def compute_reread_counts(student_data):
     """"
-    This program shows how many times students had to reread the text depending
-    on the context (ad or short story) and the question.
+    Given a list of student response dicts,
+    return a matrix containing the number of times students had to reread
+    the text based on the context and question.
+    :param student_data: list, student response dicts
+    :return: matrix, rows being context & question, columns having tallies
+    of how many students reread 1 time, 2 times, etc.
     """
-
     q1_ad = []
     q1_short = []
     q2_ad = []
@@ -64,39 +65,42 @@ def run_analysis():
                 q3_short.append(view_count)
 
 
-q1_final_ad = []
-q2_final_ad = []
-q3_final_ad = []
-q1_final_short = []
-q2_final_short = []
-q3_final_short = []
-zero_counter = 0
-one_counter = 0
-two_counter = 0
-three_counter = 0
-four_counter = 0
-five_above= 0
+    q1_final_ad = []
+    q2_final_ad = []
+    q3_final_ad = []
+    q1_final_short = []
+    q2_final_short = []
+    q3_final_short = []
+    zero_counter = 0
+    one_counter = 0
+    two_counter = 0
+    three_counter = 0
+    four_counter = 0
+    five_above= 0
 
-for x in q1_ad:
-    if(x==0):
-        zero_counter+=1
-    elif(x==1):
-        one_counter+=1
-    elif (x == 2):
-        two_counter += 1
-    elif (x == 3):
-        three_counter += 1
-    elif (x == 4):
-        four_counter += 1
-    else:
-        five_above +=1
+    for x in q1_ad:
+        if(x==0):
+            zero_counter+=1
+        elif(x==1):
+            one_counter+=1
+        elif (x == 2):
+            two_counter += 1
+        elif (x == 3):
+            three_counter += 1
+        elif (x == 4):
+            four_counter += 1
+        else:
+            five_above +=1
 
-
-
-
+    return results  # We need to format our output
 
 
-    print(student_data)
+def run_analysis():
+    csv_path = Path('data', 'rereading_data_2019-09-13.csv')
+    student_data = load_data_csv(csv_path)
+    reread_counts = compute_reread_counts(student_data)
+    print("Number of times students reread text based on question or context:\n")
+    print(reread_counts)
 
 
 if __name__ == '__main__':

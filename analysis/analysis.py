7e72c6016091e7609ecfb8deeeab94f44dedd656
Christina Wang <mingyew@mit.edu>
2019-09-20 15:37:24 -0400

    update function/documentation

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -27,46 +27,51 @@ def load_data_csv(csv_path: Path):
     return out_data
 
 
+def extract_one_word_response(student_data, context):
+    one_word_response = []
+    for dictionary in student_data:
+        if (dictionary["context"] == context
+                and dictionary["question"] == "In one word, how does this text make you feel?"):
+            one_word_response.append(dictionary["response"])
+    return one_word_response
+
+
+def extract_one_word_views(student_data, context):
+    views = []
+    for dictionary in student_data:
+        if (dictionary["context"] == context
+                and dictionary["question"] == "In one word, how does this text make you feel?"):
+            views.append(len(dictionary["views"]))
+    return views
+
+
+def count_sad(responses):
+    count = 1
+    for r in responses:
+        if r.lower() == "sad":
+            count += 1
+    return count
+
+
 def run_analysis():
     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
     student_data = load_data_csv(csv_path)
-    # TODO: do something with student_data that's not just printing it
-    one_word_response = []
-    story_response = []
-    response_dictionary = {}
-    for dictionary in student_data:
-        if (dictionary["context"] == "This is an ad."
-                and dictionary["question"] == "In one word, how does this text make you feel?"):
-            one_word_response.append(dictionary["response"])
-            len_views = len(dictionary["views"])
-            if len_views == 0:
-                print('0')
-            elif len_views in response_dictionary.keys():
-                response_dictionary[len_views].append(dictionary["response"])
-            else:
-                response_dictionary.update({len_views: [dictionary["response"]]})
-        if (dictionary["context"] == "This is actually a short story."
-                and dictionary["question"] == "In one word, how does this text make you feel?"):
-            story_response.append(dictionary["response"])
-    one_word_response_sad_number = 0
-    story_response_sad_number = 0
-    for response in one_word_response:
-        if response.lower() == "sad":
-            one_word_response_sad_number += 1
-    for response in story_response:
-        if response.lower() == "sad":
-            story_response_sad_number += 1
-    print(one_word_response)
-    print(one_word_response_sad_number)
-    print(story_response)
-    print(story_response_sad_number)
-    views = []
-    for dictionary in student_data:
-        views.append(len(dictionary["views"]))
-    print(views)
-    print("max views", max(views))
-    print("average views:", sum(views) / len(views))
-    print(response_dictionary)
+
+    one_word_response_ad = extract_one_word_response(student_data, "This is an ad.")
+    one_word_response_story = extract_one_word_response(student_data, "This is actually a short story.")
+    count_sad_ad = count_sad(one_word_response_ad)
+    count_sad_story = count_sad(one_word_response_story)
+    print("Given the context of an ad, the number of one-word responses to the story that say 'sad' is", count_sad_ad)
+    print("In comparison, given the context of a story, the number is", count_sad_story)
+    print()
+
+    one_word_views_ad = extract_one_word_views(student_data, "This is an ad.")
+    one_word_views_story = extract_one_word_views(student_data, "This is actually a short story.")
+    print("Given the context of an ad, the average number of views taken to give a one-word response is",
+          '{0:.3g}'.format(sum(one_word_views_ad) / len(one_word_views_ad)))
+    print("In comparison, given the context of a story, the average number is",
+          '{0:.3g}'.format(sum(one_word_views_story) / len(one_word_views_story)))
+
     # print(student_data)
 
 

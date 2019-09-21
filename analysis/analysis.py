a2562c696930161314c4a903cca36161d17b56c8
Christina Wang <mingyew@mit.edu>
2019-09-21 00:40:29 -0400

    updated functions and added docstrings

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -6,6 +6,7 @@ Analysis.py - initial analyses for dhmit/rereading
 from ast import literal_eval
 import csv
 from pathlib import Path
+import statistics
 
 
 def load_data_csv(csv_path: Path):
@@ -27,77 +28,155 @@ def load_data_csv(csv_path: Path):
     return out_data
 
 
-def detect_string_at_index(base_string, start_index, string_segment):
-    if start_index > len(base_string):
-        return False
-    elif len(string_segment) == 0:
-        return True
-    if not start_index >= len(base_string):
-        if base_string[start_index] == string_segment[0]:
-            return detect_string_at_index(base_string, start_index + 1, string_segment[1:])
-        elif base_string[start_index] != string_segment[0]:
-            return False
-    else:
-        return False
-
-
-def find(sub_string, main_string):
-    index = 0
-    found_indexes = []
-    while index < len(main_string):
-        sub_string_found = detect_string_at_index(main_string, index, sub_string)
-        if sub_string_found:
-          found_indexes.append(index)
-        index+=1
-    return found_indexes
-
-
-def extract_one_word_response(student_data, context):
+def extract_response(student_data, question, context):
+    """
+    Takes a dataset, a specific question, and a context
+    returns all responses that correspond to them
+    :param student_data: list, student response dicts
+    :param question: str, question of interest
+    :param context: str, context of interest
+    :return: list, all responses corresponding to the question & context
+    """
     response = []
     for dictionary in student_data:
         if (dictionary["context"] == context
-                and dictionary["question"] == "In one word, how does this text make you feel?"):
+                and dictionary["question"] == question):
             response.append(dictionary["response"])
     return response
 
 
-def extract_one_word_views(student_data, context):
+def extract_views(student_data, question, context):
+    """
+    Takes a dataset, a specific question, and a context
+    returns the number of views for each response that correspond to them
+    :param student_data: list, student response dicts
+    :param question: str, question of interest
+    :param context: str, context of interest
+    :return: list, the number of views for each response
+    """
     views = []
     for dictionary in student_data:
         if (dictionary["context"] == context
-                and dictionary["question"] == "In one word, how does this text make you feel?"):
+                and dictionary["question"] == question):
             views.append(len(dictionary["views"]))
     return views
 
 
-def count_sad(responses):
-    count = 1
+def count_word(responses, word):
+    """
+    Takes a list and a specific string
+    returns the number of entries that contain the string
+    :param responses: list[str]
+    :param word: str, string or word of interest
+    :return: int, the number of entries that contain the string
+    """
+    count = 0
     for r in responses:
-        if r.lower() == "sad":
+        if word in r.lower():
             count += 1
     return count
 
 
-def run_analysis():
-    csv_path = Path('data', 'rereading_data_2019-09-13.csv')
-    student_data = load_data_csv(csv_path)
+def analyze_word_count(student_data, question, word):
+    """
+    Takes a dataset, a specific question, and a word
+    returns the number of responses that contain the word from the ad context & the story context respectively
+    :param student_data: list, student response dicts
+    :param question: str, question of interest
+    :param word: str, word of interest
+    :return: tuple, contains the number of responses that contain the word in the ad context
+    and the number of responses in the story context
+    """
+    response_ad = extract_response(student_data, question, "This is an ad.")
+    response_story = extract_response(student_data, question, "This is actually a short story.")
+    count_ad = count_word(response_ad, word)
+    count_story = count_word(response_story, word)
+    return count_ad, count_story
 
-    one_word_response_ad = extract_one_word_response(student_data, "This is an ad.")
-    one_word_response_story = extract_one_word_response(student_data, "This is actually a short story.")
-    count_sad_ad = count_sad(one_word_response_ad)
-    count_sad_story = count_sad(one_word_response_story)
-    print("Given the context of an ad, the number of one-word responses to the story that say 'sad' is", count_sad_ad)
-    print("In comparison, given the context of a story, the number is", count_sad_story)
+
+def print_analysis_word_count(student_data, question, word):
+    """
+    Takes a dataset, a specific question, and a word
+    prints the number of responses that contain the word for the ad context vs. the story context
+    :param student_data: list, student response dicts
+    :param question: str, question of interest
+    :param word: str, word of interest
+    :return: None
+    """
+    result = analyze_word_count(student_data, question, word)
+    print(f'For the question: "{question}":')
+    print(f"Given the context of an ad, the number of responses that include '{word}' is {result[0]}")
+    print(f"In comparison, given the context of a story, the number is {result[1]}")
     print()
 
-    one_word_views_ad = extract_one_word_views(student_data, "This is an ad.")
-    one_word_views_story = extract_one_word_views(student_data, "This is actually a short story.")
-    print("Given the context of an ad, the average number of views taken to give a one-word response is",
-          '{0:.3g}'.format(sum(one_word_views_ad) / len(one_word_views_ad)))
-    print("In comparison, given the context of a story, the average number is",
-          '{0:.3g}'.format(sum(one_word_views_story) / len(one_word_views_story)))
 
-    # print(student_data)
+def print_total_analysis_word_count(student_data):
+    """
+    Takes a dataset,
+    prints the comparison between ad and story context
+    number of times "sad" appeared in one-word responses & number of times "shoe" appeared in three-word responses
+    :param student_data: list, student response dicts
+    :return: None
+    """
+    print_analysis_word_count(student_data, "In one word, how does this text make you feel?", "sad")
+    print_analysis_word_count(student_data, "In three words or fewer, what is this text about?", "shoe")
+
+
+def print_analysis_views(student_data, question):
+    """
+    Takes a dataset and a specific question,
+    prints the statistical summaries on the number of views people took for the question for ad & story contexts
+    :param student_data: list, student response dicts
+    :param question: str, question of interest
+    :return: None
+    """
+    views_ad = extract_views(student_data, question, "This is an ad.")
+    views_story = extract_views(student_data, question, "This is actually a short story.")
+    print(f'For the question: "{question}":')
+    print(f"Given the context of an ad, the average number of views taken to give a response is "
+          f"{'{0:.3g}'.format(sum(views_ad) / len(views_ad))}")
+    print(f"(Min: {min(views_ad)}, Max: {max(views_ad)}, Median: {statistics.median(views_ad)})")
+    print(f"In comparison, given the context of a story, the average number is "
+          f"{'{0:.3g}'.format(sum(views_story) / len(views_story))}")
+    print(f"(Min: {min(views_story)}, Max: {max(views_story)}, Median: {statistics.median(views_story)})")
+    print()
+
+
+def print_total_analysis_views(student_data):
+    """
+    Takes a dataset, prints the analysis results on the number of views (compared between ad & story context)
+    :param student_data: list, student response dicts
+    :return: None
+    """
+    print_analysis_views(student_data, "In one word, how does this text make you feel?")
+    print_analysis_views(student_data, "In three words or fewer, what is this text about?")
+
+
+def load_data():
+    """
+    Loads data from csv file
+    :return: list[dict], student data
+    """
+    csv_path = Path('data', 'rereading_data_2019-09-13.csv')
+    return load_data_csv(csv_path)
+
+
+def print_analysis(data):
+    """
+    Takes data, prints analyses from word count and number of views
+    :param data: list, student response dicts
+    :return: None
+    """
+    print_total_analysis_word_count(data)
+    print_total_analysis_views(data)
+
+
+def run_analysis():
+    """
+    Runs the whole analysis
+    :return: None
+    """
+    print_analysis(load_data())
 
 
 if __name__ == '__main__':

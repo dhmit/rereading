c36b3eb0a8515cd564c83c9a92576671cc03d1ea
Samantha York <samimak@mit.edu>
2019-10-04 15:38:36 -0400

    Update analysis.py

--- backend/apps/readings/analysis.py
+++ backend/apps/readings/analysis.py
@@ -4,6 +4,64 @@ Analysis.py - analyses for dhmit/rereading wired into the webapp
 
 """
 from .models import StudentResponse
+from pathlib import Path
+
+
+def get_sentiments() -> dict:
+    """
+    Returns a dictionary of sentiment scores, with the keys being the word and the values being
+    their score
+
+    :return: dict mapping words to their sentiment scores
+    """
+    sentiment_path = Path('rereading', 'analysis', 'data', 'sentiments.txt')
+
+    sentiments = dict()
+    with open(sentiment_path, 'r') as file:
+        word = file.readline()
+
+        # We want to handle each word individually, rather than as a whole set
+        while word:
+
+            # This particular file starts lines with '#' for non-sentiment comments,
+            # so skip them
+            if word[0] == '#' or word[0] == '\t':
+                word = file.readline()
+                continue
+
+            # All words use tabs to define the different parts of the data
+            attributes = word.split('\t')
+
+            # Pull out the word from the line
+            data = attributes[4]
+            data = data.split('#')
+            new_word = data[0]
+            positive_score = float(attributes[2])
+            negative_score = float(attributes[3])
+
+            # If the word is already in the dictionary, pick the larger value
+            # This is not optimal, but standardizes data
+            if new_word in sentiments:
+                if abs(sentiments[new_word]) > abs(positive_score) and abs(
+                    sentiments[new_word]) > \
+                    abs(negative_score):
+                    word = file.readline()
+                    continue
+
+            # Find the largest sentiment score for the word, and define negative sentiments
+            # as negative values (if there's a tie, the sentiment is 0)
+            if positive_score == negative_score:
+                score = 0
+            elif positive_score > negative_score:
+                score = float(positive_score)
+            else:
+                score = -float(negative_score)
+
+            sentiments[new_word] = score
+
+            word = file.readline()
+
+    return sentiments
 
 
 class RereadingAnalysis:

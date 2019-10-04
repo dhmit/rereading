ca6fb4d4bb015292d3a774fa86febe32a0edcc70
Peter Choza Tone <ptone@mit.edu>
2019-10-04 19:37:18 -0400

    Reverted to PR-ready file
    Reverted to commit 19ac4d5 to undo merge conflict bad resolution.

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -26,85 +26,79 @@ def load_data_csv(csv_path: Path):
             out_data.append(row)
     return out_data
 
-# def collect_word_frequencies(responses):
-#     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
-#     student_data = load_data_csv(csv_path)
-#
-#     response_ad = []
-#     response_story = []
-#
-#     for x in student_data:
-#         if x['question'] == "In three words or fewer, what is this text about?" and x['context'] == "This is an ad.":
-#             response_ad.append(x['response'])
-#         elif x['question'] == "In three words or fewer, what is this text about?" and x['context'] == "This is actually a short story.":
-#             response_story.append(x['response'])
-#     print(response_ad)
-#     print(response_story)
-#
-#     response_dict_ad = {}
-#     response_dict_story = {}
-#
-#     for response in response_ad:
-#
-#
-#         if "ad" in response[2]:
-#             response_dict_ad[response[3]] = response_dict_ad.get(response[3], 0) + 1
-#
-#             else:
-#             response_dict_story[response[3]] = response_dict_story.get(response[3], 0) + 1
-#
-#     return response_dict_ad
-#     return response_dict_story
 
-# analysis of verbs versus nouns in ad responses and story responses - OLD ANALYSIS PROGRAM
-
-def run_analysis():
+def repeated_prompt_words():
+    """
+    Reports frequencies of exact words from story prompt used in responses. Prints analysis
+    :return: tuple (response_ad, response_story), lists of unique responses to be used in further analysis.
+    """
     stop_words = ['a', 'and', 'the', 'of', 'an', 'for']
     csv_path = Path('data', 'rereading_data_2019-09-13.csv')
     student_data = load_data_csv(csv_path)
     # TODO: do something with student_data that's not just printing it!
+
+    # extracting text responses from csv file, depending on context given (story vs. ad)
     response_ad = []
     response_story = []
+    question = "In three words or fewer, what is this text about?"
     for x in student_data:
-        x['response'] = x['response'].replace('.', '').lower()
+        x['response'] = x['response'].replace('.', '').lower()  # removing punctuation and making string lowercase
         x['response'] = x['response'].replace(',', '')
-        if x['question'] == "In three words or fewer, what is this text about?" and x['context'] == "This is an ad.":
+        if x['question'] == question and x['context'] == "This is an ad.":
             response_ad += x['response'].split()
-        elif x['question'] == "In three words or fewer, what is this text about?" and x['context'] == "This is actually a short story.":
+        elif x['question'] == question and x['context'] == "This is actually a short story.":
             response_story += x['response'].split()
 
+    # dictionaries to store frequencies of words in responses based on context given
+    # word (string) : frequency (int)
     ad_resp_words = {}
     story_resp_words = {}
-    for word in response_ad:
 
+    # cycling through ad responses
+    for word in response_ad:
         if word in ad_resp_words:
             ad_resp_words[word] += 1
         elif word not in stop_words:
             ad_resp_words[word] = 1
-    # change to story responses
+
+    # cycling through story responses
     for word in response_story:
         if word in story_resp_words:
             story_resp_words[word] += 1
         elif word not in stop_words:
             story_resp_words[word] = 1
-    # count the words in the response that repeat the original story
+
+    # list of words in story - for longer story, would want to read it from csv file using list comprehension
     story_vocab_list = ["for", "sale", "baby", "shoes", "never", "worn"]
-    # for the first question with the context of the ad
+
+    # for the first question given the context of the ad
     print("Given text vocab words that occurred in ad-context responses: ")
     for word in story_vocab_list:
         if word in ad_resp_words:
             print(word, ad_resp_words[word])
-    # for the 2nd question with the context of the story
+
+    # for the second question given the context of the story
     print("Given text vocab words that occurred in story-context responses: ")
     for word in story_vocab_list:
         if word in story_resp_words:
             print(word, story_resp_words[word])
-#
+
+    # uncomment for most commonly repeated words
+
+    # print("Most commonly repeated words in ad context response:")
+    # print(max(ad_resp_words.items(), key=lambda item: item[1]))
+    # print("Most commonly repeated words in story context response:")
     # print(max(story_resp_words.items(), key=lambda item: item[1]))
 
+    # uncomment for complete list of words used in responses (unsorted)
+
+    # print("All words in ad context responses:")
     # print(ad_resp_words)
+    # print("All words in story context responses:")
     # print(story_resp_words)
+
+    return response_ad, response_story
 
 
 if __name__ == '__main__':
-    run_analysis()
+    repeated_prompt_words()

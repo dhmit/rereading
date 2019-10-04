5fdc0be15c03b4a2d9cc584da994abb68b1a8eb2
Ivy Mao <maoivy01@gmail.com>
2019-10-04 17:41:44 -0400

    Fixed backend and made result display on analysis (though hard-coded)

--- backend/apps/readings/analysis.py
+++ backend/apps/readings/analysis.py
@@ -6,6 +6,49 @@ Analysis.py - analyses for dhmit/rereading wired into the webapp
 from .models import StudentResponse
 
 
+def get_responses_for_question(all_responses, question):
+    """
+    For a certain question, returns the set of responses as a dictionary with keys being the
+    context and values being nested dictionaries containing each response and their frequency.
+    :param all_responses: QuerySet of all student responses
+    :param question: string, question
+    :return: dictionary mapping strings to integers
+    """
+    responses = {}
+    for response in all_responses:
+        student_question = response.question.text
+        student_answer = response.response.lower()
+        question_context = response.context.text
+        if student_question == question:
+            if question_context not in responses:
+                responses[question_context] = {student_answer: 1}
+            else:
+                if student_answer in responses[question_context]:
+                    responses[question_context][student_answer] += 1
+                else:
+                    responses[question_context][student_answer] = 1
+    return responses
+
+
+def most_common_response(all_responses, question, context):
+    """
+    Returns a list of the most common response(s) given a set of data, a question,
+    and a context.
+    :param all_responses: student response object
+    :param question: string, question
+    :param context: string, context
+    :return: list of strings
+    """
+    max_response = []
+    response_dict = get_responses_for_question(all_responses, question)
+    responses_by_context = response_dict[context]
+    max_response_frequency = max(responses_by_context.values())
+    for response in responses_by_context:
+        if responses_by_context[response] == max_response_frequency:
+            max_response.append(response)
+    return max_response
+
+
 class RereadingAnalysis:
     """
     This class loads all student responses from the db,
@@ -35,72 +78,7 @@ class RereadingAnalysis:
 
     @property
     def get_most_common_responses_for_all(self):
-        most_common_responses = {
-            "In one word, how does this text make you feel?": most_common_response(self.responses,
-                                                                                   "In one word, "
-                                                                                   "how does this "
-                                                                                   "text make you "
-                                                                                   "feel?",
-                                                                                   "This is an "
-                                                                                   "ad."),
-            "In three words or fewer, what is this text about?": most_common_response(
-                self.responses,
-                "In three "
-                "words or "
-                "fewer, "
-                "what is "
-                "this text "
-                "about?",
-                "This is an "
-                "ad."),
-            "Have you encountered this text before?": most_common_response(self.responses,
-                                                                           "Have you "
-                                                                           "encountered "
-                                                                           "this text "
-                                                                           "before?",
-                                                                           "This is an ad.")
-        }
+        question = "In one word, how does this text make you feel?"
+        context = "This is an ad."
+        most_common_responses = {question: most_common_response(self.responses, question, context)}
         return most_common_responses
-
-
-def get_responses_for_question(student_response, question):
-    """
-    For a certain question, returns the set of responses as a dictionary with keys being the
-    context and values being nested dictionaries containing each response and their frequency.
-    :param student_response: student response object
-    :param question: string, question
-    :return: dictionary mapping strings to integers
-    """
-    responses = {}
-    for response in student_response.responses:
-        student_question = response.question.text
-        student_response = response.response.lower()
-        question_context = response.context.text
-        if student_question == question:
-            if question_context not in responses:
-                responses[question_context] = {student_response: 1}
-            else:
-                if student_response in responses[question_context]:
-                    responses[question_context][student_response] += 1
-                else:
-                    responses[question_context][student_response] = 1
-    return responses
-
-
-def most_common_response(student_response, question, context):
-    """
-    Returns a list of the most common response(s) given a set of data, a question,
-    and a context.
-    :param student_response: student response object
-    :param question: string, question
-    :param context: string, context
-    :return: list of strings
-    """
-    max_response = []
-    response_dict = student_response.get_responses_for_question(question)
-    responses_by_context = response_dict[context]
-    max_response_frequency = max(responses_by_context.values())
-    for response in responses_by_context:
-        if responses_by_context[response] == max_response_frequency:
-            max_response.append(response)
-    return max_response

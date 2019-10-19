3c0cac33b9c6476595d84a58bc35e3d47afbdd78
Ninger Lei <ninger.lei@gmail.com>
2019-10-18 22:25:50 -0400

    Update analysis_view.js

--- frontend/src/prompted_readings/analysis_view.js
+++ frontend/src/prompted_readings/analysis_view.js
@@ -36,7 +36,53 @@ class AnalysisView extends React.Component {
                     <h1>Analysis of Student Responses</h1>
                     <h3>Total view time</h3>
                     <p>{total_view_time} seconds</p>
+
+                    <table>
+                        <tr>
+                            <th>{0}</th>
+                            <th>{1}</th>
+                            <th>{2}</th>
+                            <th>{3}</th>
+                        </tr>
+                        <tr>
+                            <td>{this.state.reread_counts.getKey("In one word," +
+                                " how" +
+                                " does this make you feel?").getKey("This is an" +
+                                " ad.")}</td>
+                            <td>{this.state.analysis.reread_counts().getKey("In one word, how" +
+                                " does this make you feel?").getKey("This is an" +
+                                "ad").getValue(0)}</td>
+                            <td>{this.state.analysis.reread_counts().getKey("In one word, how" +
+                                " does this make you feel?").getKey("This is an" +
+                                "ad").getValue(1)}</td>
+                            <td>{this.state.analysis.reread_counts().getKey("In one word, how" +
+                                " does this make you feel?").getKey("This is an" +
+                                "ad").getValue(2)}</td>
+                            <td>{this.state.analysis.reread_counts().getKey("In one word, how" +
+                                " does this make you feel?").getKey("This is an" +
+                                "ad").getValue(3)}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.state.analysis.reread_counts().getKey("In one word, how" +
+                                " does this make you feel?").getKey("This is a" +
+                                " ashort story.")}</td>
+                            <td>{this.state.analysis.reread_counts().getKey("In one word, how" +
+                                " does this make you feel?").getKey("This is a short" +
+                                "story.").getValue(0)}</td>
+                            <td>{this.state.analysis.reread_counts().getKey("In one word, how" +
+                                " does this make you feel?").getKey("This is a short" +
+                                "story.").getValue(1)}</td>
+                            <td>{this.state.analysis.reread_counts().getKey("In one word, how" +
+                                " does this make you feel?").getKey("This is a short" +
+                                "story.").getValue(2)}</td>
+                            <td>{this.state.analysis.reread_counts().getKey("In one word, how" +
+                                " does this make you feel?").getKey("This is a short" +
+                                "story.").getValue(3)}</td>
+                        </tr>
+                    </table>
                 </div>
+
+
             );
         } else {
             return (
@@ -44,6 +90,36 @@ class AnalysisView extends React.Component {
             );
         }
     }
+
+
 }
+
+/*
+
+
+
+*    renderTableHeader() {
+      let header = Object.keys(this.state.students[0])
+      return header.map((key, index) => {
+         return <th key={index}>{key.toUpperCase()}</th>
+      })
+   }
+
+   render() {
+      return (
+         <div>
+            <h1 id='title'>React Dynamic Table</h1>
+            <table id='students'>
+               <tbody>
+                  <tr>{this.renderTableHeader()}</tr>
+                  {this.renderTableData()}
+               </tbody>
+            </table>
+         </div>
+      )
+   }
+*
+* */
+
 
 export default AnalysisView;

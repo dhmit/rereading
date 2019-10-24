46483ba887611040b25be253670550e4e865eb1c
Ophelia <wattinytoffee@gmail.com>
2019-10-24 17:31:14 -0400

    Basic Reading View Layout
    Included url link to reading view, rendered title, prompts, text, and button with functionality.

--- frontend/src/prompted_readings/reading_view.js
+++ frontend/src/prompted_readings/reading_view.js
@@ -1,9 +1,12 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
 const document = {
-    title: "test_title",
+    title: "The Pigs",
     prompts: ["the author is alive", "the book was written during the cold war"],
     segments: [
         {
-            text: "",
+            text: "The little pig built a house.",
             seq: 0,
             prompts: ["this is an ad"],
             questions: [
@@ -20,7 +23,7 @@ const document = {
             ]
         },
         {
-            text: "test",
+            text: "The wolf huffed and puffed",
             seq: 1,
             prompts: ["this is a story", "the author is testing you"],
             questions: [
@@ -37,7 +40,7 @@ const document = {
             ]
         },
         {
-            text: "test2",
+            text: "The pigs have brick house so they are safe.",
             seq: 2,
             prompts: ["this is a poem"],
             questions: [
@@ -55,4 +58,50 @@ const document = {
         },
     ]
 };
-console.log(document);
+
+class TextSegments extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            segmentNum: 0,
+        }
+    }
+
+    changeSegment (changeNum) {
+        const newNum = this.state.segmentNum + changeNum;
+        if (newNum >= 0 && newNum < this.props.data.segments.length){
+            this.setState({segmentNum: newNum});
+        }
+    }
+
+    render() {
+        const data = this.props.data;
+        return (
+            <div className={"container"}>
+                <h1>{data.title}</h1>
+                <p><b>Prompts: </b>{data.prompts.map(el => "[" + el + "] ")}</p>
+                <p>Segment Number: {this.state.segmentNum + 1}</p>
+                <p>{data.segments[this.state.segmentNum].text}</p>
+                <button onClick = {() => this.changeSegment(-1)}>Back</button>
+                <button onClick = {() => this.changeSegment(1)}>Next</button>
+            </div>
+        )
+    }
+}
+
+TextSegments.propTypes = {
+    data: PropTypes.object,
+};
+
+class ReadingView extends React.Component {
+    render() {
+        return (
+            <div className={"container"}>
+                <TextSegments data={document}/>
+            </div>
+        );
+    }
+}
+
+export default ReadingView;

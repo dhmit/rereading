d86f9220434cb8fba4f874a3aacd5173cee4a114
Amy <leiamy12@gmail.com>
2019-10-25 17:00:39 -0400

    Initial styling - changed header and button

--- frontend/src/views/reading_view.js
+++ frontend/src/views/reading_view.js
@@ -83,12 +83,12 @@ class ReadingView extends React.Component {
         const data = document;
         return (
             <div className={"container"}>
-                <h1>{data.title}</h1>
+                <h1 className={"display-4 py-3 pr-3"}>{data.title}</h1>
                 <p><b>Prompts: </b>{data.prompts.map(el => "[" + el + "] ")}</p>
                 <p>Segment Number: {this.state.segmentNum + 1}</p>
                 <p>{data.segments[this.state.segmentNum].text}</p>
-                <button onClick = {() => this.changeSegment(-1)}>Back</button>
-                <button onClick = {() => this.changeSegment(1)}>Next</button>
+                <button className={"btn btn-outline-dark mr-2"} onClick = {() => this.changeSegment(-1)}>Back</button>
+                <button className={"btn btn-outline-dark"} onClick = {() => this.changeSegment(1)}>Next</button>
             </div>
         );
     }

3e8365317bb19315af664dd1720d5447aaf66625
Ivy Mao <maoivy01@gmail.com>
2019-10-25 17:45:44 -0400

    Added toggle cold read

--- frontend/src/views/reading_view.js
+++ frontend/src/views/reading_view.js
@@ -61,6 +61,7 @@ const document = {
     ]
 };
 
+// const annotations = []
 
 
 class ReadingView extends React.Component {
@@ -68,6 +69,7 @@ class ReadingView extends React.Component {
         super(props);
         this.state = {
             segmentNum: 0,
+            showing: false,
         }
     }
 
@@ -77,19 +79,47 @@ class ReadingView extends React.Component {
         if (newNum >= 0 && newNum < document.segments.length){
             this.setState({segmentNum: newNum});
         }
+        if (this.state.showing === true) {
+            this.setState({showing: !this.state.showing});
+        }
     }
+
+    // addAnnotation (startIndex, endIndex) {
+    //
+    // }
 
     render() {
         const data = document;
+        const questions = data.segments[this.state.segmentNum].questions;
+        const segment = data.segments[this.state.segmentNum].text;
+
         return (
             <div className={"container"}>
                 <h1>{data.title}</h1>
-                <p><b>Prompts: </b>{data.prompts.map(el => "[" + el + "] ")}</p>
                 <p>Segment Number: {this.state.segmentNum + 1}</p>
-                <p>{data.segments[this.state.segmentNum].text}</p>
+                <p>{segment}</p>
                 <button onClick = {() => this.changeSegment(-1)}>Back</button>
                 <button onClick = {() => this.changeSegment(1)}>Next</button>
+                <button onClick = {() => this.setState({ showing: !this.state.showing })}>
+                    Toggle analysis view</button>
+
+
+                { this.state.showing &&
+                    <div className={"analysis"}>
+                        <p><b>Prompts: </b>{data.prompts.map(el => "[" + el + "] ")}</p>
+                        <p><b>Questions: </b>{questions.map(el => "[" + el.question + "] ")}</p>
+                        <p>{segment}</p>
+                        <p><b>Add an annotation: </b><input
+                            type="text"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                        /><button>Submit</button></p>
+                    </div>
+                }
             </div>
+            /* idea: maybe show the text again and allow highlighting and annotation (which
+               would show on mouse hover using popover
+             */
         );
     }
 }

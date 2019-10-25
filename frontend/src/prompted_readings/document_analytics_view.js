1b982f52295ecb8a13e5b3ef098840571b6d89e2
Felix Li <felixli5678@gmail.com>
2019-10-25 17:08:50 -0400

    added new files

---
+++ frontend/src/prompted_readings/document_analytics_view.js
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import {
+    CommonResponses, ContextVsViewTime,
+    FrequencyFeelingTable,
+    MeanReadingTimesForQuestions, SentimentScores
+} from "./analysis_view";
+
+export class DocumentAnalysisView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // we initialize analysis to null, so we can check in render() whether
+            // we've received a response from the server yet
+            analysis: null,
+        };
+    }
+
+    /**
+     * This function is fired once this component has loaded into the DOM.
+     * We send a request to the backend for the analysis data.
+     */
+    async componentDidMount() {
+        try {
+            const response = await fetch('/api/analysis/');
+            const analysis = await response.json();
+            this.setState({analysis});
+        } catch (e) {
+            // For now, just log errors to the console.
+            console.log(e);
+        }
+    }
+
+    render() {
+        if (this.state.analysis !== null) {
+            const {
+
+            } = this.state.analysis;
+            return (
+                <div className={"container"}>
+                    <nav className={"navbar navbar-expand-lg"}>
+                        <div className={"navbar-nav"}>
+                            <a
+                                className={"nav-link nav-item text-dark font-weight-bold"}
+                                href={"#"}
+                            >Overview</a>
+                            <a
+                                className={"nav-link nav-item text-dark font-weight-bold"}
+                                href={"#"}
+                            >Analysis</a>
+                        </div>
+                    </nav>
+                    <h1
+                        className={"text-center display-4"}
+                        id={"page-title"}
+                    >Analysis of Student Responses</h1>
+
+                </div>
+            );
+        } else {
+            return (
+                <div>Loading!</div>
+            );
+        }
+    }
+}

57ff28d901e34db7d0b9278e4ee2a8b4a00a19d8
Ryaan Ahmed <rahmed@mit.edu>
2019-10-31 12:08:40 -0400

    fix urls, rename 'document_analytics_view'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import StudentView from './views/student_view';
 import InstructorView from './views/instructor_view';
-import {AnalysisView} from './views/analysis_view';
-import {DocumentAnalysisView} from "./views/document_analytics_view";
+import { AnalysisView } from './views/analysis_view';
+import { DocumentAnalysisView } from "./views/document_analysis_view";
 import ReadingView from './views/reading_view';
 
 window.app_modules = {

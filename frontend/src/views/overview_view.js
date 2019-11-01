094fe408cdef7e5dae6ed0c6e806e06fdaa8894d
Ivy Mao <maoivy01@gmail.com>
2019-11-01 17:42:56 -0400

    got all segments to work

--- frontend/src/views/overview_view.js
+++ frontend/src/views/overview_view.js
@@ -1,5 +1,26 @@
 import React from "react";
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+
+class OverviewWindow extends React.Component {
+    render() {
+        return (
+            <div className={"row"}>
+                <div className={"col-8"}>
+                    {this.props.all_segments.map((el, i) => (
+                        <p key={i}>{el.text}</p>)
+                    )}
+                </div>
+                <div className={"col-4"}>
+                    <p>hello</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+OverviewWindow.propTypes = {
+    all_segments: PropTypes.array,
+};
 
 class OverviewView extends React.Component {
     constructor(props){
@@ -29,103 +50,19 @@ class OverviewView extends React.Component {
 
     }
 
-    prevSegment () {
-        // document will be replaced by actual data
-        if (this.state.segmentNum > 0){
-            this.setState({segmentNum: this.state.segmentNum-1, rereading: true, overview: false});
-        }
-    }
-
-    nextSegment () {
-        const length = this.state.document.segments.length;
-        if (this.state.segmentNum < length - 1){
-            if (this.state.rereading) {
-                // If we're already rereading, move to the next segment
-                this.setState({rereading: false, segmentNum: this.state.segmentNum+1});
-            } else {
-                // Otherwise, move on to the rereading layout
-                this.setState({rereading: true});
-            }
-        } else if (this.state.segmentNum === length - 1){
-            this.setState({overview: true});
-        }
-    }
-
 
     render() {
         const data = this.state.document;
 
         if (data) {
-            const current_segment = data.segments[this.state.segmentNum];
-            const segment_text = current_segment.text;
-            const segment_lines = segment_text.split("\r\n");
-            const segment_questions = current_segment.questions;
-            const segment_contexts = current_segment.contexts;
+            const all_segments = data.segments;
             // const document_questions = data.questions;
 
             return (
                 <div className={"container"}>
                     <h1 className={"display-4 py-3 pr-3"}>{data.title}</h1>
-                    <div className={"row"}>
-                        <div className={"col-8"}>
-                            <p>Segment Number: {this.state.segmentNum + 1}</p>
-                            {segment_lines.map((line, k) => (
-                                <p key={k}>{line}</p>)
-                            )}
-                            <button
-                                className={"btn btn-outline-dark mr-2"}
-                                onClick = {() => this.prevSegment()}
-                            >
-                                Back
-                            </button>
-                            <button
-                                className={"btn btn-outline-dark"}
-                                onClick = {() => this.nextSegment()}
-                            >
-                                {this.state.rereading ? 'Next' : 'Reread'}
-                            </button>
-                        </div>
+                    <OverviewWindow all_segments={all_segments} />
 
-                        {this.state.rereading &&
-                            <div className={"analysis col-4"}>
-                                <p><b>Context: </b></p>
-                                <p>
-                                    {segment_contexts.map((el,i) =>
-                                        <ul key={i}>
-                                            <li>{el.text}</li>
-                                        </ul>)}
-                                </p>
-                                <p><b>Questions: </b></p>
-                                <p>
-                                    {segment_questions.map((el,i) =>
-                                        <ul key={i}>
-                                            <li>{el.text}</li>
-                                        </ul>
-                                    )}
-                                </p>
-                                <p>
-                                    <b>Add an annotation: </b><input
-                                        type="text"
-                                        value={this.state.value}
-                                        onChange={this.handleChange}
-                                    /><button>Submit</button>
-                                </p>
-                            </div>
-                        }
-
-                        {this.state.overview &&
-                            <div className={"analysis col-4"}>
-                                <p><b>Document Questions: </b></p>
-                                <p>
-                                    {/*{document_questions.map((el,i) =>*/}
-                                    {/*    <ul key={i}>*/}
-                                    {/*        <li>{el.text}</li>*/}
-                                    {/*    </ul>)}*/}
-                                    filler
-                                </p>
-                            </div>
-                        }
-                    </div>
                 </div>
             );
         } else {

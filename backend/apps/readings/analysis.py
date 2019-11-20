60faa77c01915dda64c16648c27dd8ba9fa07e68
Melissa Calvert <mcalvert@mit.edu>
2019-11-20 17:11:53 -0500

    Backend Implementation
    Contains analysis and serializer.
    Untested functionality.
    Still needs to be incorporated in the frontend.

--- backend/apps/readings/analysis.py
+++ backend/apps/readings/analysis.py
@@ -3,8 +3,10 @@
 Analysis.py - analyses for dhmit/rereading wired into the webapp
 
 """
+import statistics
+from collections import defaultdict
 
-from .models import StudentReadingData
+from .models import StudentReadingData, StudentSegmentData
 
 
 class RereadingAnalysis:
@@ -15,5 +17,23 @@ class RereadingAnalysis:
 
     def __init__(self):
         self.readings = StudentReadingData.objects.all()
+        self.segments = StudentSegmentData.objects.all()
+
+    def total_and_median_view_time(self):
+        total_time = 0
+        ret_dict = defaultdict(list)
+        for segment in self.segments:
+            view_time = segment.view_time
+            ret_dict[segment.reading_data] = view_time
+            total_time += view_time
+        student_total_view_time = []
+        if not ret_dict.values():
+            median_view_time = 0
+        else:
+            for item in ret_dict.values():
+                student_total_view_time.append(sum(item))
+            median_view_time = statistics.median(student_total_view_time)
+        ret_tuple = (round(total_time), round(median_view_time))
+        return ret_tuple
 
 

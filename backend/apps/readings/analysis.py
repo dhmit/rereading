1c0ab1fd8e9b1118faeec1893dbfa5ffa9a41378
Melissa Calvert <mcalvert@mit.edu>
2019-11-21 17:02:10 -0500

    Frontend Implementation
    Frontend implementation of total view times and median view times are working. May need further testing.

--- backend/apps/readings/analysis.py
+++ backend/apps/readings/analysis.py
@@ -19,22 +19,32 @@ class RereadingAnalysis:
         self.segments = StudentSegmentData.objects.all()
 
     def total_and_median_view_time(self):
-        total_time = 0
+        """
+        This function totals the overall view times and calculates the median view time per student
+        :return: a tuple containing (total view time, median view time per student)
+        """
+        total_time = 0  # track the total reading time while looking at segment times
         ret_dict = {}
+        in_dict = []  # prevent calling ret_dict.keys() repeatedly
         for segment in self.segments:
+            # prime two variable to reduce calls to database
             view_time = segment.view_time
             reading_data = segment.reading_data
-            if reading_data not in ret_dict.keys():
+            if reading_data not in in_dict:
                 ret_dict[reading_data] = [view_time]
+                in_dict.append(reading_data)
             else:
                 ret_dict[reading_data].append(view_time)
             total_time += view_time
         student_total_view_time = []
+        # do not cycle through something that doesn't exist
         if not ret_dict.values():
             median_view_time = 0
         else:
             for item in ret_dict.values():
+                # total the segment view times for each student
                 student_total_view_time.append(sum(item))
+            # find the mean of the total reading time for each student
             median_view_time = statistics.median(student_total_view_time)
         ret_tuple = (round(total_time), round(median_view_time))
         return ret_tuple

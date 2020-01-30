6637b399a6e9e6530340590505bf65c1efe8494e
Ubuntu <ubuntu@ip-172-31-19-184.us-west-1.compute.internal>
2020-01-30 20:04:38 +0000

    w

---
+++ assets/precache-manifest.94c2ca9de6332708c33a4ccfddcb58b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff7f8f1b18a7437c81786896980ddcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a368071a857798c389b",
+    "url": "/static/css/main.21533d1b.chunk.css"
+  },
+  {
+    "revision": "345302197830fbf9e354",
+    "url": "/static/js/2.42e00f78.chunk.js"
+  },
+  {
+    "revision": "6a368071a857798c389b",
+    "url": "/static/js/main.2de0589c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
